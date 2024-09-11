c55e44b52f230ba3fbfb1349ea5b234656c0a51c
Kale Bharat <147477477+Kalebharat@users.noreply.github.com>
2024-09-11 17:05:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle Navigation Menu
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
